2465686aafa089df975e84c6ce4d23e4587015ad
Vinicius Gouvea <vrgouvea@outlook.com>
2024-04-13 18:51:24 -0300

    first commit

---
+++ src/http/server.ts
@@ -0,0 +1,10 @@
+import Elysia from "elysia";
+
+const app = new Elysia()
+  .get('/', () => {
+    return "hello world"
+  })
+
+app.listen(3333, () => {
+  console.log("🔥 Http server running")
+})
