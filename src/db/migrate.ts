e41ce2f91f9bd46cc9a8e0446059074f4b9d7ed5
Vinicius Gouvea <vrgouvea@outlook.com>
2024-04-13 21:02:16 -0300

    drizzle migrations

---
+++ src/db/migrate.ts
@@ -0,0 +1,13 @@
+import postgres from "postgres";
+
+import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+
+const connection = postgres('postgresql://docker:docker@localhost5432/pizzashop', { max: 1 })
+const db = drizzle(connection)
+
+await migrate(db, { migrationsFolder: 'drizzle'})
+
+await connection.end()
+
+process.exit()
