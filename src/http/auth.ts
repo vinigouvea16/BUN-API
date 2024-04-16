accd2c0566c135f819532b9f680cdb6c7a6a5183
Vinicius Gouvea <vrgouvea@outlook.com>
2024-04-16 19:00:52 -0300

    get profile route created

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,13 +1,45 @@
 import jwt from '@elysiajs/jwt'
-import Elysia, { t } from 'elysia'
+import { Elysia, t, type Static } from 'elysia'
 import { env } from '../env'
 
-export const auth = new Elysia().use(
-  jwt({
-    secret: env.JWT_SECRET_KEY,
-    schema: t.Object({
-      sub: t.String(),
-      restaurantId: t.Optional(t.String()),
+const jwtPayload = t.Object({
+  sub: t.String(),
+  restaurantId: t.Optional(t.String()),
+})
+
+export const auth = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: jwtPayload,
     }),
-  }),
-)
+  )
+  .derive({ as: 'scoped' }, ({ jwt, cookie: { auth } }) => {
+    return {
+      signUser: async (payload: Static<typeof jwtPayload>) => {
+        const token = await jwt.sign(payload)
+
+        auth.value = token
+        auth.httpOnly = true
+        auth.maxAge = 60 * 60 * 24 * 7 // 7 days
+        auth.path = '/'
+      },
+
+      signOut: async () => {
+        auth.remove()
+      },
+
+      getCurrentUser: async () => {
+        const payload = await jwt.verify(auth.value)
+
+        if (!payload) {
+          throw new Error('Unauthorized.')
+        }
+
+        return {
+          userId: payload.sub,
+          restaurantId: payload.restaurantId,
+        }
+      },
+    }
+  })
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,11 +2,15 @@ import { Elysia } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { signOut } from './routes/sign-out'
+import { getProfile } from './routes/get-profile'
 
 const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
+  .use(signOut)
+  .use(getProfile)
 
 app.listen(3333, () => {
   console.log('🔥 Http server running')
