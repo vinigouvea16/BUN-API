75ba7c1658ce0a0a9ff0b1a813ac298a96fa8c46
Vinicius Gouvea <vrgouvea@outlook.com>
2024-04-17 10:48:46 -0300

    products & orders schemas

---
+++ src/db/schema/orders.ts
@@ -0,0 +1,29 @@
+import { text, timestamp, pgTable, integer, pgEnum } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
+import { restaurants } from './restaurants'
+import { users } from './users'
+
+export const orderStatusEnum = pgEnum('order_status', [
+  'pending',
+  'processing',
+  'delivering',
+  'delivered',
+  'canceled',
+])
+
+export const orders = pgTable('orders', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  customerId: text('customer_id').references(() => users.id, {
+    onDelete: 'set null',
+  }),
+  restaurantId: text('restaurant_id')
+    .notNull()
+    .references(() => restaurants.id, {
+      onDelete: 'cascade',
+    }),
+  status: orderStatusEnum('status').default('pending').notNull(),
+  totalInCents: integer('total_in_cents').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+})
