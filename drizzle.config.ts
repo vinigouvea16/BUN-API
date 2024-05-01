2ca6eed81ab3d3512ee397cefeebf4989a0f7190
Vinicius Gouvea <vrgouvea@outlook.com>
2024-05-01 11:30:25 -0300

    drizzle-kit update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "drizzle-kit";
+import type { Config } from 'drizzle-kit'
 
 export default {
-  schema: "./src/db/schema/index.ts",
-  out: "./drizzle",
-  driver: 'pg', 
+  schema: './src/db/schema/index.ts',
+  out: './drizzle',
+  driver: 'pg',
   dbCredentials: {
     connectionString: 'postgresql://docker:docker@localhost:5432/pizza_shop',
   },
-} satisfies Config;
+} satisfies Config
